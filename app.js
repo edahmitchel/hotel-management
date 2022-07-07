e370f3a355f29c52cb133be6a504b3f1f64af59a
TYDev01 <anthonychinaemeru84@yahoo.com>
2022-07-08 00:38:14 +0100

    Add new file

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-let firstName = document.querySelector("#inputName").value;
-let lastName = document.querySelector("#inputLastName").value;
-let userNumber = document.querySelector("#inputNumber").value;
-let userAddress = document.querySelector("#inputAddress").value;
-let nextofKin = document.querySelector("#inputNextOfKin").value;
-let nexofkinNumber = document.querySelector("#inputNextOfKinNumber").value;
-let gender = document.querySelector("#inputState").value;
-console.log(firstName)
-console.log(lastName)
-console.log(userNumber)
-console.log(userAddress)
-console.log(nextofKin)
-console.log(nexofkinNumber)
-console.log(gender)
+let display = document.querySelector("#ourlisted")
+const firstName = document.querySelector("#inputName").value
+const lastName = document.querySelector("#inputLastName").value
+const userNumber = document.querySelector("#inputNumber").value
+let userAddress = document.querySelector("#inputAddress").value
+let nextofKin = document.querySelector("#inputNextOfKin").value
+let nexofkinNumber = document.querySelector("#inputNextOfKinNumber").value
+let gender = document.querySelector("#inputState").value
+let random = Math.floor(Math.random() * 1000);
+console.log(display)
 
+//STORING ON LOCAL STORAGE
 let fname = localStorage.setItem("firstName", firstName)
 let lname = localStorage.setItem("lastName", lastName)
 let userNum = localStorage.setItem("userNumber", userNumber)
 let UserAdd = localStorage.setItem("userAddress", userAddress)
 let nexKin = localStorage.setItem("nextofKin", nextofKin)
 let nexKinNum = localStorage.setItem("nexofkinNumber", nexofkinNumber)
-let gend = localStorage.setItem("gender", gender)
+let gend = localStorage.setItem("gender", gender);
+
+
+//GETTING FROM LOCAL STORAGE
+let fgname = localStorage.getItem("firstName", firstName)
+let lgname = localStorage.getItem("lastName", lastName)
+let ugserNum = localStorage.getItem("userNumber", userNumber)
+let UgserAdd = localStorage.getItem("userAddress", userAddress)
+let ngexKin = localStorage.getItem("nextofKin", nextofKin)
+let ngexKinNum = localStorage.getItem("nexofkinNumber", nexofkinNumber)
+let ggend = localStorage.getItem("gender", gender);
+
+let gotten = [fgname,lgname,ugserNum,UgserAdd,ngexKin,ngexKinNum,ggend];
+
+console.log(gotten[3])
+
+function displayingTheStoredItems(theDetails) {
+    let items = ""
+    for(let i = 0; i <= theDetails.length; i++) {
+        items += `<li>${theDetails[i]}</li>`;
+    }
+    return items;
+}
+
+console.log(displayingTheStoredItems(gotten))
+
+
+// display.textContent = gotten;
