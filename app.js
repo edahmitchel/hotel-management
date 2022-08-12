26a2a802fe52460d21ec1cc4184877141ca7a151
TYDev01 <anthonychinaemeru84@yahoo.com>
2022-08-12 01:49:06 +0100

    General changes

--- app.js
+++ app.js
@@ -5,8 +5,57 @@ const userAddress = document.querySelector("#inputAddress").value;
 const nextofKin = document.querySelector("#inputNextOfKin").value;
 const nexofkinNumber = document.querySelector("#inputNextOfKinNumber").value;
 const submitBtn = document.querySelector('#submitBtn')
-const gender = document.querySelector("#inputState").value;
+const yourNam = document.querySelector("#yourNam")
+const yourNumber = document.querySelector("#yourNumber")
+const resvPass = document.querySelector("#resvPass")
+const total = document.querySelector("#total")
+const days = document.querySelector("#days").value;
+console.log(days)
+// const gender = document.querySelector("#inputState").value;
 const random = Math.floor(Math.random() * 1000);
+
+const amount = 999;
+tot = amount * days;
+console.log(tot)
+submitBtn.addEventListener('click', e => {
+    e.preventDefault();
+    let theData = JSON.stringify({
+        fname: firstName,
+        lname: lastName,
+        userNum: userNumber,
+        userAdd: userAddress,
+        nextofKin: nextofKin,
+        dayz: days*1,
+        nexkinNum: nexofkinNumber
+        // gend: gender 
+    })
+    const amount = 999;
+    tot = amount * days;
+    console.log(tot)
+localStorage.setItem('theData', theData)
+localStorage.getItem('theData', theData);
+    if (localStorage == "") {
+        console.log("empty")
+    }else{
+        localStorage.clear();
+        localStorage.setItem('theData', theData)
+        displayForm()
+        console.log("done")
+    }
+    // let arr = [];
+    // arr.push(theData)
+    // localStorage.setItem('data', JSON.stringify(theData))
+    
+})
+
+function displayForm(){
+    let gotIt = JSON.parse(localStorage.getItem('theData'))
+    yourNam.textContent = "Name: " + gotIt.fname + " " + gotIt.lname;
+    yourNumber.textContent = "Phone Number: " + gotIt.userNum;
+    // console.log(gotIt)
+}
+
+
 
 //DISPLAY GOTTEN
 let yourName = document.querySelector("#yourName")
@@ -17,22 +66,32 @@ let keym = document.querySelector("#keym")
 let randd = Math.trunc(Math.random() * 10000);
 // console.log(display)
 
-submitBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    let theData = {
-        fname: firstName,
-        lname: lastName,
-        userNum: userNumber,
-        userAdd: userAddress,
-        nextofKin: nextofKin,
-        nexkinNum: nexofkinNumber,
-        gend: gender 
-    }
-    let locale = localStorage.setItem('formData', JSON.stringify(theData))
-    // console.log(locale);
-    displayForm();
-    
-});
+// submitBtn.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     let theData = {
+//         fname: firstName,
+//         lname: lastName,
+//         userNum: userNumber,
+//         userAdd: userAddress,
+//         nextofKin: nextofKin,
+//         nexkinNum: nexofkinNumber,
+//         gend: gender 
+//     }
+
+//     if (theData.value === null) {
+//         console.log("empty")
+//     }else{
+//         console.log("it's plenty");
+//     }
+//     // console.log(locale);
+//     displayForm();
+//     // document.querySelector("#inputNextOfKinNumber").value = " ";
+//     // document.querySelector("#inputName").value = " ";
+//     // document.querySelector("#inputLastName").value = " ";
+//     // document.querySelector("#inputNumber").value = " ";
+//     // document.querySelector("#inputAddress").value = " ";
+//     // document.querySelector("#inputNextOfKin").value = " "; 
+// });
 
 
 
@@ -42,11 +101,7 @@ submitBtn.addEventListener('click', (event) => {
     document.querySelector("#inputNumber").value = " ";
     document.querySelector("#inputAddress").value = " ";
     document.querySelector("#inputNextOfKin").value = " ";*/
-function displayForm(){
-    let gotIt = JSON.parse(localStorage.getItem('formData'))
-    yourName.textContent = gotIt.fname;
-    // console.log(gotIt)
-}
+
 
 //DISPLAYING THE FORM
 
@@ -117,4 +172,18 @@ console.log(JSON.parse(localStorage.getItem("appData", sendData)))*/
 // let akatsuki = document.querySelector('akatsuki');
 
 // akatsuki.textContent = parsedDATA
+
+
+
+/* let getLocal = localStorage.getItem('formData');
+
+    if (getLocal == null) {
+        itemsObj = [];
+    }else{
+        itemsObj = JSON.parse(FormData);
+    }
+
+    itemsObj.push(theData.value)
+
+    let locale = localStorage.setItem('formData', JSON.stringify(theData)) */
 
